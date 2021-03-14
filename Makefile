6f452f370c18e15ce52ec3cb3365063bff068594
Anjia Wang <anjiawang@gmail.com>
2021-03-14 00:54:12 +0000

    Add a demo implementation of task graph generator

--- Makefile
+++ Makefile
@@ -2,8 +2,8 @@ ifeq (${REX_INSTALL},)
 	REX_INSTALL := ${REX_ROOT}/rex_install
 endif
 
-default: main.cpp
-	g++ main.cpp -I${REX_INSTALL}/include/rose -L${REX_INSTALL}/lib -lrose -lboost_system -o pfg.out
+default: main.cpp ./flow_graph/flow_graph.cpp
+	g++ main.cpp ./flow_graph/flow_graph.cpp -I ./flow_graph -I${REX_INSTALL}/include/rose -L${REX_INSTALL}/lib -lrose -lboost_system -o pfg.out
 
 clean: 
 	rm -rf *.o *.out rose*
--- main.cpp
+++ main.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
 #include "rose.h"
+#include "flow_graph.h"
 
 using namespace SageBuilder;
 using namespace SageInterface;
 
 int main (int argc, char *argv[]) {
-  SgProject* project = frontend(argc, argv);
-  SgGlobal* global = getFirstGlobalScope(project);
 
-  SgFunctionDeclaration* main_func= findMain(project);
-  SgBasicBlock* body= main_func->get_definition()->get_body();
+    // generate the REX AST
+    SgProject* project = frontend(argc, argv);
+    SgGlobal* global = getFirstGlobalScope(project);
+
+    SgFunctionDeclaration* main_func= findMain(project);
+    SgBasicBlock* body= main_func->get_definition()->get_body();
   
-  AstTests::runAllTests(project);
-  project->unparse();
+    AstTests::runAllTests(project);
+    project->unparse();
 
-  return 0;
+    // generate a dummy task graph
+    Node* root = generate_dummy_graph();
+
+    return 0;
 }
 
