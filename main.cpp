79f813349e21aee3711101247f993a1981cad1f2
Kewei <ambipom_yan@outlook.com>
2021-03-30 07:36:31 +0000

    Remove visit function from main

--- main.cpp
+++ main.cpp
@@ -7,24 +7,6 @@ extern void visualize(SgOmpFlowGraphNode*);
 using namespace SageBuilder;
 using namespace SageInterface;
 
-void visit(SgOmpFlowGraphNode* root) {
-
-    std::cout << "SgNode: " << root->get_node()->sage_class_name() << " at line: " << root->get_node()->get_startOfConstruct()->get_line() << "\n";
-
-    // TODO: visualize a single graph node
-    visualize_node(root);
-
-    std::list<SgNode* > children = root->get_children();
-    if (children.size()) {
-        std::list<SgNode* >::iterator iter;
-        for (iter = children.begin(); iter != children.end(); iter++) {
-            SgOmpFlowGraphNode* child = ((SgOmpFlowGraphNode*)(*iter));
-            SgNode* node = child->get_node();
-            visit(child);
-        };
-    };
-}
-
 
 int main (int argc, char *argv[]) {
 
@@ -36,7 +18,7 @@ int main (int argc, char *argv[]) {
 
     // list all statements in the task graph
     printf("Check the parallel flow graph....\n");
-    visit(root);
+    //visit(root);
 
     // TODO: visualize the whole graph and store it to a DOT file
     visualize(root);
