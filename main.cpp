38f2ffd841b680454428dfad44aca341a4912837
Anjia Wang <anjiawang@gmail.com>
2021-04-27 00:43:36 +0000

    Use the dummy visualization for faster development

--- main.cpp
+++ main.cpp
@@ -7,6 +7,23 @@ extern void visualize(SgOmpFlowGraphNode*);
 using namespace SageBuilder;
 using namespace SageInterface;
 
+void visit(SgOmpFlowGraphNode* root) {
+
+    std::cout << "SgNode: " << root->get_node()->sage_class_name() << " at line: " << root->get_node()->get_startOfConstruct()->get_line() << ", cost: " << root->get_cost() << "\n";
+
+    // TODO: visualize a single graph node
+    visualize_node(root);
+
+    std::list<SgNode* > children = root->get_children();
+    if (children.size()) {
+        std::list<SgNode* >::iterator iter;
+        for (iter = children.begin(); iter != children.end(); iter++) {
+            SgOmpFlowGraphNode* child = ((SgOmpFlowGraphNode*)(*iter));
+            SgNode* node = child->get_node();
+            visit(child);
+        };
+    };
+}
 
 int main (int argc, char *argv[]) {
 
@@ -18,10 +35,10 @@ int main (int argc, char *argv[]) {
 
     // list all statements in the task graph
     printf("Check the parallel flow graph....\n");
-    //visit(root);
+    visit(root);
 
     // TODO: visualize the whole graph and store it to a DOT file
-    visualize(root);
+    //visualize(root);
 
     return 0;
 }
